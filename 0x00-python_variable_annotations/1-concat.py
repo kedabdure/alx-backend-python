133f990609908fa326fcc0b99d41210a2426ad21
kedabudre <abdurehimk77@gmail.com>
2024-08-15 21:33:22 +0300

    create 1-concat.py that concatenate two string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+type-annotated function
+'''
+
+
+import string
+
+
+def concat(str1: string, str2: string) -> string:
+    """concatenate to string"""
+    return str1 + str2
