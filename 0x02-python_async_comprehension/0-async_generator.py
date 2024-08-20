d6ba1dceed1113d103c5a2234c2148d24d1f8c55
kedabudre <abdurehimk77@gmail.com>
2024-08-20 20:36:54 +0200

    create 0-async_generator.py README.md

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+0-async_generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
